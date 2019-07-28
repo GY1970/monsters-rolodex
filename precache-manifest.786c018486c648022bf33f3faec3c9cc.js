d742dbf2fd4b79cb3f19399e8da646260ea4597d
GY1970 <gy1970@yahoo.com>
2019-07-27 21:45:48 -1000

    Updates

---
+++ precache-manifest.786c018486c648022bf33f3faec3c9cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c3fb8db1a41a4269f6ab7501547faf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aca7becbf846116fc170",
+    "url": "/monsters-rolodex/static/css/main.a1269ac6.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "aca7becbf846116fc170",
+    "url": "/monsters-rolodex/static/js/main.0a65e8d9.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
